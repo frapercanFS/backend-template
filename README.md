2d42f65f0e8ceea9e94bffbe15e375d98fc235e3
xaxi <francisco.perez@thebridgeschool.es>
2022-06-04 13:25:58 +0200

    Hello World + Cors

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 NodeJS + Express + MongoDB plantilla para comenzar proyectos
 
 # Inicializar Proyecto
-
 > npm init
 
-# Dependencias
+# Habilitar Express
 
 > npm install express --save
+> Copiar el código de muestra de Express (Hello World)
+
+
+# Habilitar peticiones cruzadas
+
+> npm install cors --save
+> Habilitandolo en index.js
+
+
+# Cómo instalar las dependencias? (Trás clonar el repositorio y no tener los node_modules instalados)
+> npm install //(Esto instalará todas las dependencias que han sido instaladas con --save automáticamente)
+
+# Cómo lanzar el proyecto?
+
+> npm start
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var cors = require("cors");
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/prueba', function(req, res) {
-    var post_body = req.body;
-    res.send(post_body);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
